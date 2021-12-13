979dd444c07f179f4cde240fb90b180278343088
Fahri Meral <fahrimeral@outlook.com>
2021-12-13 20:31:00 +0300

    Add: localization for media upload (lang)

--- install-stubs/resources/js/admin/app-components/Form/AppUpload.js
+++ install-stubs/resources/js/admin/app-components/Form/AppUpload.js
@@ -1,5 +1,34 @@
 import { BaseUpload } from 'craftable';
 
 Vue.component('media-upload', {
-    mixins: [BaseUpload]
+    mixins: [BaseUpload],
+    template: `<dropzone :id="collection"
+                       :url="url"
+                       :language="{
+                                dictDefaultMessage : 'Dosyaları yüklemek için buraya bırakın',
+                                dictFallbackMessage : 'Tarayıcınız sürükle bırak dosya yüklemelerini desteklemiyor.',
+                                dictFallbackText : 'Dosyalarınızı eski günlerdeki gibi yüklemek için lütfen aşağıdaki geri dönüş formunu kullanın.',
+                                dictFileTooBig : 'Dosya çok büyük ({{filesize}}MiB). Maksimum Dosya Boyutu: {{maxFilesize}}MiB.',
+                                dictInvalidFileType : 'Bu türdeki dosyaları yükleyemezsiniz.',
+                                dictResponseError : 'Sunucu yanıt verdi {{statusCode}} kodu.',
+                                dictCancelUpload : 'Yüklemeyi iptal et',
+                                dictCancelUploadConfirmation : 'Bu yüklemeyi iptal etmek istediğinizden emin misiniz?',
+                                dictRemoveFile : 'Dosyayı kaldır',
+                                dictMaxFilesExceeded : 'Daha fazla dosya yükleyemezsiniz.',
+                           }"
+                       v-bind:preview-template="template"
+                       v-on:vdropzone-success="onSuccess"
+                       v-on:vdropzone-error="onUploadError"
+                       v-on:vdropzone-removed-file="onFileDelete"
+                       v-on:vdropzone-file-added="onFileAdded"
+                       :useFontAwesome="true"
+                       :ref="collection"
+                       :maxNumberOfFiles="maxNumberOfFiles"
+                       :maxFileSizeInMB="maxFileSizeInMb"
+                       :acceptedFileTypes="acceptedFileTypes"
+                       :thumbnailWidth="thumbnailWidth"
+                       :headers="headers">
+
+                <input type="hidden" name="collection" :value="collection">
+            </dropzone>`
 });
